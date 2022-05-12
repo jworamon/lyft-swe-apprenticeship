3c76c6b395a6aa33b2e08cf98aa77bdf92199bad
Woramon Jamjod <jworamon@gmail.com>
2022-05-12 12:54:50 -0400

    added unit test for cutString

---
+++ tests/util.spec.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const { cutString } = require('../server/util');
+
+describe('cutString', () => {
+    it('returns a string', () => {
+        expect(cutString('xyz')).to.be.a('string');
+    });
+
+    it('returns a string that contains every third letter from the original string', () => {
+        expect(cutString('iamyourlyftdriver')).to.equal('muydv');
+        expect(cutString('hellowold')).to.equal('lwd');
+    });
+
+    it('returns an empty string if the original string length is less than 3', () => {
+        expect(cutString('')).to.equal('');
+        expect(cutString('a')).to.equal('');
+        expect(cutString('aa')).to.equal('');
+    });
+});
+
